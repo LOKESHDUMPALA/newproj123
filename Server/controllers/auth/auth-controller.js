7c1f58c9cd3af46f69e023d571fc17d1f2a8f2a5
LOKESH DUMPALA <133016649+LOKESHDUMPALA@users.noreply.github.com>
2025-02-12 20:28:59 +0530

    Update auth-controller.js

--- Server/controllers/auth/auth-controller.js
+++ Server/controllers/auth/auth-controller.js
@@ -68,29 +68,18 @@ const loginUser = async (req, res) => {
       { expiresIn: "60m" }
     );
 
-    // res.cookie("token", token, { httpOnly: true, secure: true }).json({
-    //   success: true,
-    //   message: "Logged in successfully",
-    //   user: {
-    //     email: checkUser.email,
-    //     role: checkUser.role,
-    //     id: checkUser._id,
-    //     userName: checkUser.userName,
-    //   },
-    // });
-
-    res.status(200).json({
-      success : true,
-      message : "Logged in successfully",
-      token,
-       user: {
+    res.cookie("token", token, { httpOnly: true, secure: true }).json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
         email: checkUser.email,
         role: checkUser.role,
         id: checkUser._id,
         userName: checkUser.userName,
       },
     });
-   
+
+
     
   } catch (e) {
     console.log(e);
